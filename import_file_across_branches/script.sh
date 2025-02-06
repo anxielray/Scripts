99d21b3d85d73d92f0a7985d6bccb6005f514551
anxielray <anxielworld@gmail.com>
2025-02-06 17:31:49 +0300

    [Import-file across branches]: Modify the script to make the particular file executable

--- import_file_across_branches/script.sh
+++ import_file_across_branches/script.sh
@@ -1,5 +1,6 @@
 #!bin/bash
 
+echo "provide 4 arguments 1; repository url 2; remote branch to clone 3; file 4; local file"
 # Fetch the remote branch; [$1]: Repository name. [$2]: remote branch name
 git fetch $1 $2
 
@@ -8,3 +9,7 @@ COMMIT_HASH=$(git rev-parse origin/$2)
 
 # Get the file from the remote branch and write into a new file [$3]: The name of the file that you want to copy. [$4]: The new local file.
 git show $COMMIT_HASH:$3 > $4
+
+# Make the script executable
+chmod 777 $4
+
