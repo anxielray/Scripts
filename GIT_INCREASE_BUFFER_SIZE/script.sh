83396cf5fea3d7a732b1647125d3fb136086ccb0
anxielray <anxielworld@gmail.com>
2026-03-12 22:11:10 -0400

    Add script to increase buffer size sent to the remote :

---
+++ GIT_INCREASE_BUFFER_SIZE/script.sh
@@ -0,0 +1,10 @@
+# for 100MB
+git config --global http.maxBuffer 100M
+
+# for 500MB
+git config --global http.maxBuffer 524288000
+
+# git push origin <branch> --force --verbose
+
+# this script is not advised if you have very huge data.
+# My advice, use the GIT_SPLIT_PUSH/script.sh
